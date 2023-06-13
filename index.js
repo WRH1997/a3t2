56e488c4bb94d3a75a7ecbea7b895b4f155f185d
Waleed Alhindi <wl392785@dal.ca>
2023-06-13 11:38:39 -0300

    dayF

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fetch = require('node-fetch');
 const app = express();
 const ip = require('ip')
 const parser = require('body-parser');
@@ -14,7 +15,20 @@ app.use(parser.json());
 
 app.get('/test', (req, res) => {
   let ip1 = ip.address();
-  res.json({'data':'YYYsomeData', "req":req.body, "ip":ip1});
+  fetch("http://192.168.2.14:5000/incoming", {
+    method: "POST",
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({"data":"someData"})
+  }).then((ret) => {
+    return ret.json();
+  }).then((json)=>{
+    console.log(json);
+    res.json(json);
+  })
+  //res.json({'data':'YYYsomeData', "req":req.body, "ip":ip1});
 })
 
 
@@ -22,5 +36,5 @@ const port = 6000;
 
 //https://expressjs.com/en/starter/hello-world.html
 app.listen(port, () => {
-console.log(`Server 1 started on ${port}`)
+  console.log(`Server 1 started on ${ip.address()}:${port}`)
 })
