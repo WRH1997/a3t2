1c6ddde1528dc54c10c88ae4426c8ed82ccbb972
Waleed Alhindi <wl392785@dal.ca>
2023-06-13 09:35:50 -0300

    t

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,8 @@
-FROM node:lts-jessie
-
-# Set the work directory
-RUN mkdir -p /var/www/app
-WORKDIR /var/www/app
-
-# Add our package.json and install *before* adding our application files
-ADD app/package.json ./
+From node:latest
+WORKDIR /app1
+ADD package.json ./
 RUN npm i --production
-
-# Install pm2 *globally* so we can run our application
 RUN npm i -g pm2
-
-# Add application files
-ADD app /var/www/app
-
-EXPOSE 4000
-
+COPY . .
+EXPOSE 6000
 CMD ["pm2", "start", "process.json", "--no-daemon"]
---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const ip = require('ip')
+const parser = require('body-parser');
+const fs = require('fs');
+const axios = require('axios');
+var path = require('path');
+
+app.use(parser.urlencoded({
+  extended: true,
+}))
+app.use(parser.json());
+
+
+app.get('/test', (req, res) => {
+  console.log(req);
+  res.json({'data':'someData', "req":req, "ip":ip.address()});
+})
+
+
+const port = 6000;
+
+//https://expressjs.com/en/starter/hello-world.html
+app.listen(port, () => {
+console.log(`Server 1 started on ${port}`)
+})
